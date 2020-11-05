e24bc2f688d44c0645e7cbe64cefa30a4c8f2a59
Mateusz Surjak <surjak1999@gmail.com>
2020-11-05 20:00:12 +0100

    mandelbort set

--- TW/src/com/surjak/lab5/Main.java
+++ TW/src/com/surjak/lab5/Main.java
@@ -1,5 +1,6 @@
 package com.surjak.lab5;
 
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -8,6 +9,25 @@ import java.util.concurrent.ExecutionException;
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Mandelbrot mandelbrot = new Mandelbrot();
-        mandelbrot.setVisible(true);
+        mandelbrot.run();
+        mandelbrot.printTimeMap();
+
+        System.out.println("****************************************");
+
+        Mandelbrot mandelbrot2 = new Mandelbrot();
+        mandelbrot2.setFinalTaskCount(100);
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100);
+        mandelbrot2.setNumOfThreadsArray(integers);
+        mandelbrot2.run();
+        mandelbrot2.printTimeMap();
+
+        System.out.println("****************************************");
+
+        Mandelbrot mandelbrot3 = new Mandelbrot();
+        mandelbrot3.setExactNumOfThreadsAndTasks(true);
+        mandelbrot3.setNumOfThreadsArray(integers);
+        mandelbrot3.run();
+        mandelbrot3.printTimeMap();
+
     }
 }
--- TW/src/com/surjak/lab5/Mandelbrot.java
+++ TW/src/com/surjak/lab5/Mandelbrot.java
@@ -1,49 +1,65 @@
 package com.surjak.lab5;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.awt.image.BufferedImage;
+import java.util.List;
 import java.util.concurrent.*;
 import javax.swing.JFrame;
 
 public class Mandelbrot extends JFrame {
 
     private final int MAX_ITER = 1570;
-    private final double ZOOM = 1500;
-    private final int numOfThreads = 100;
+    private final double ZOOM = 500;
+    private int numOfThreads = 10;
     private BufferedImage I;
-    private final int taskCount = numOfThreads * 10;
+    private int taskCount = numOfThreads * 10;
+    private List<String> timeList = new ArrayList<>();
+    private List<Integer> numOfThreadsArray = List.of(1, 2, 5, 10, 20, 25, 50, 100);
+    private boolean finalTaskCount = false;
+    private boolean exactThreadsAndTasks = false;
 
     public Mandelbrot() throws ExecutionException, InterruptedException {
         super("Mandelbrot Set");
         setBounds(100, 100, 1000, 1000);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        I = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
-
-
-        ThreadPoolExecutor es = (ThreadPoolExecutor) Executors.newFixedThreadPool(numOfThreads);
-        List<Runnable> tasks = new ArrayList<>();
-        for (int i = 1; i <= taskCount; i++) {
-            int height = getHeight() / taskCount * i;
-            tasks.add(() -> drawPartOfImage(height, getHeight() / taskCount));
-        }
-        CompletableFuture<?>[] futures = tasks.stream()
-                .map(task -> CompletableFuture.runAsync(task, es))
-                .toArray(CompletableFuture[]::new);
-
-        CompletableFuture.allOf(futures).join();
-        es.shutdown();
-
-
     }
+
+    public void run() {
+        for (int j : numOfThreadsArray) {
+            numOfThreads = j;
+            if (!finalTaskCount)
+                taskCount = numOfThreads * 10;
+            if(exactThreadsAndTasks)
+                taskCount = j;
+
+            I = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+
+            long startTimeMillis = System.currentTimeMillis();
+            ThreadPoolExecutor es = (ThreadPoolExecutor) Executors.newFixedThreadPool(numOfThreads);
+            List<Runnable> tasks = new ArrayList<>();
+            for (int i = 1; i <= taskCount; i++) {
+                int height = getHeight() / taskCount * i;
+                tasks.add(() -> drawPartOfImage(height, getHeight() / taskCount));
+            }
+            CompletableFuture<?>[] futures = tasks.stream()
+                    .map(task -> CompletableFuture.runAsync(task, es))
+                    .toArray(CompletableFuture[]::new);
+
+            CompletableFuture.allOf(futures).join();
+            es.shutdown();
+            long endTimeMillis = System.currentTimeMillis();
+            timeList.add("For " + numOfThreads + " threads and " + taskCount + " tasks: " + (endTimeMillis - startTimeMillis) + " milliseconds");
+        }
+    }
+
 
     private void drawPartOfImage(int height, int dy) {
 
         for (int y = height - dy; y < height; y++) {
             for (int x = 0; x < getWidth(); x++) {
-                double zx =0;
+                double zx = 0;
                 double zy = 0;
                 double cX = (x - 400) / ZOOM;
                 double cY = (y - 300) / ZOOM;
@@ -61,10 +77,30 @@ public class Mandelbrot extends JFrame {
         }
     }
 
+    public void printTimeMap() {
+        timeList.stream()
+                .forEach(System.out::println);
+    }
+
     @Override
     public void paint(Graphics g) {
         g.drawImage(I, 0, 0, this);
     }
 
 
+    public void setFinalTaskCount(int i) {
+        this.taskCount = i;
+        this.finalTaskCount = true;
+    }
+
+    public void setNumOfThreadsArray(List<Integer> numOfThreadsArray) {
+        this.numOfThreadsArray = numOfThreadsArray;
+    }
+
+    public void setExactNumOfThreadsAndTasks(boolean exact) {
+        if(exact && finalTaskCount){
+            throw new RuntimeException();
+        }
+        this.exactThreadsAndTasks = exact;
+    }
 }
